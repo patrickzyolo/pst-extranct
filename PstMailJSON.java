cb57152fd57eb0dda8ed307f9aea9aec5cac7cc7
patrickzyolo <patrick@kappa-mm.de>
2015-08-07 18:14:03 +0200

    update

--- PstMailJSON.java
+++ PstMailJSON.java
@@ -1,3 +1,6 @@
+/*
+	Java programm zum pst extrakt
+*/
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.File;
@@ -27,6 +30,7 @@ import com.independentsoft.pst.Task;
 
 public class PstMailJSON
 {
+	// Bereinigt den Namen des Attachments
 	public static String clean_name(String fileName)
 	{
 		if (fileName != null)
@@ -44,15 +48,21 @@ public class PstMailJSON
 		return fileName;
 	}
 
-	public static String convertDateToString(Date date)
+	/*
+		Konvertiert das Datum aus den funtionen
+		get_task, get_appointment und get_mail_date
+		fuer Kibana.
+
+		Manchmal wird null uebergeben
+	*/
+	public static String kibana_string_format(Date date)
 	{
 		if (date != null)
 		{
-			// SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-			String s = formatter.format(date);
+			SimpleDateFormat kibana_formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+			String kibana_Date = kibana_formatter.format(date);
 
-			return s;
+			return kibana_Date;
 		}
 		else
 		{
@@ -60,58 +70,119 @@ public class PstMailJSON
 		}
 	}
 
-	public static Map<String, Date> get_task(Item inItem)
+	/*
+		Holt die Outlook informationen ueber Tasks.
+		Datum wird direkt als Sting zurueck gegeben.
+	*/
+	public static Map<String, String> get_task(Item inItem)
 	{
-		Map<String, Date> return_task = new HashMap<String, Date>();
+		// Rueckgabe Map
+		Map<String, String> return_task = new HashMap<String, String>();
 
+		// Checkt ob Komatibel
 		if (inItem instanceof Task)
 		{
 			Task task = (Task) inItem;
 
-			return_task.put("Task StartTime", task.getStartDate());
-			return_task.put("Task EndTime", task.getDueDate());
+			Date Task_StartTime = task.getStartDate();
+			Date Task_EndTime   = task.getDueDate();
+
+			return_task.put("Task_StartTime", kibana_string_format(Task_StartTime));
+			return_task.put("Task_EndTime", kibana_string_format(Task_EndTime));
 		}
 		else
 		{
-			return_task.put("Task StartTime", null);
-			return_task.put("Task EndTime", null);
+			return_task.put("Task_StartTime", null);
+			return_task.put("Task_EndTime", null);
 		}
 		return return_task;
 	}
 
+	/*
+		Holt die Outlook informationen ueber Treffen.
+		Datum wird direkt als Sting zurueck gegeben.
+	*/
+	public static Map<String, String> get_appointment(Item inItem)
+	{
+		// Rueckgabe JsonArray
+		Map<String, String> return_appointment = new HashMap<String, String>();
+
+		// Checkt ob Komatibel
+		if (inItem instanceof Appointment)
+		{
+			Appointment appointment = (Appointment) inItem;
+
+			Date Appointment_StartTime = appointment.getStartTime();
+			Date Appointment_EndTime   = appointment.getEndTime();
+
+			return_appointment.put("Appointment_StartTime", kibana_string_format(Appointment_StartTime));
+			return_appointment.put("Appointment_EndTime", kibana_string_format(Appointment_EndTime));
+		}
+		else
+		{
+			return_appointment.put("Appointment_StartTime", null);
+			return_appointment.put("Appointment_EndTime", null);
+		}
+
+		return return_appointment;
+	}
+
+	/*
+		Gibt im JsonArray format die Entfaenger daten wieder
+
+		"name": "name",
+		"email": "email",
+		"Recipient_type": "Recipient_type"
+	*/
 	public static JSONArray get_recipier(Item inItem)
 	{
-		JSONArray return_array = new JSONArray();
+		// Rueckgabe JsonArray
+		JSONArray return_recipier = new JSONArray();
 
+		// Checkt ob Komatibel
 		if (inItem instanceof Message)
 		{
 			Message message = (Message) inItem;
-			JSONObject[] tmp = new JSONObject[message.getRecipients().size()];
 
+			/*
+				JsonObjecte (name, email, Recipient_type) fuer das JsonArray
+				Muss ein Array sein da an das Rueckgabe JsonArray nur Pointer
+				uebergeben werden.
+			*/
+			JSONObject[] recipient_obj = new JSONObject[message.getRecipients().size()];
+
+			/*
+				Alle Entfaenger -> eigenes JsonObject (recipient_obj[r])
+				Alle JsonObjecte -> JsonArray
+			*/
 			for (int r = 0; r < message.getRecipients().size(); r++)
 			{
 				Recipient recipient = message.getRecipients().get(r);
 
-				tmp[r] = new JSONObject();
+				recipient_obj[r] = new JSONObject();
 
-				tmp[r].put("Name", recipient.getDisplayName());
-				tmp[r].put("Email address", recipient.getEmailAddress());
-				tmp[r].put("Recipient type", recipient.getRecipientType().toString());
+				recipient_obj[r].put("name", recipient.getDisplayName());
+				recipient_obj[r].put("email", recipient.getEmailAddress());
+				recipient_obj[r].put("Recipient_type", recipient.getRecipientType().toString());
 
-				return_array.add(tmp[r]);
+				return_recipier.add(recipient_obj[r]);
 			}
 		}
 		else
 		{
-			return_array = null;
+			return_recipier = null;
 		}
-		return return_array;
+
+		return return_recipier;
 	}
 
-	public static Map<String, String> get_contact(Item inItem)
+	// Kontaktinformationen
+	public static JSONObject get_contact(Item inItem)
 	{
-		Map<String, String> return_contact = new HashMap<String, String>();
+		// Rueckgabe JSONObject
+		JSONObject return_contact = new JSONObject();
 
+		// Checkt ob Komatibel
 		if (inItem instanceof Contact)
 		{
 			Contact contact = (Contact) inItem;
@@ -133,52 +204,63 @@ public class PstMailJSON
 		return return_contact;
 	}
 
-	public static Map<String, Date> get_appointment(Item inItem)
-	{
-		Map<String, Date> return_appointment = new HashMap<String, Date>();
+	/*
+		funktion zum Extrahiren von Attachments
 
-		if (inItem instanceof Appointment)
-		{
-			Appointment appointment = (Appointment) inItem;
-
-			return_appointment.put("Appointment StartTime", appointment.getStartTime());
-			return_appointment.put("Appointment EndTime", appointment.getEndTime());
-		}
-		else
-		{
-			return_appointment.put("Appointment StartTime", null);
-			return_appointment.put("Appointment EndTime", null);
-		}
-
-		return return_appointment;
-	}
-
+		Gibt im JsonArray format den Attachment url
+		und asset_type wieder.
+		id muss mit gegeben werden um Dopllungen zu vermeiden.
+	*/
 	public static JSONArray get_attachments(Item inItem, long id)
 	{
+		// Daten ablage Verzeichnis
 		String dir = "Attachments/";
+
+		// Rueckgabe JsonArray
 		JSONArray return_attachments = new JSONArray();
 
+		// Checkt ob Attachments vorhanden sind.
 		if (inItem.getAttachments().size() > 0)
 		{
-			JSONObject[] tmp = new JSONObject[inItem.getAttachments().size()];
+			/*
+				JsonObject mit dem das Rueckgabe JsonArray
+				befuellt wird.
 
+				Muss ein Array sein da an das Rueckgabe JsonArray nur Pointer
+				uebergeben werden.
+			*/
+			JSONObject[] attachment_obj = new JSONObject[inItem.getAttachments().size()];
+
+			// fuer alle Attachments
 			for (int r = 0; r < inItem.getAttachments().size(); r++)
 		    {
 		    	Attachment attachment = inItem.getAttachments().get(r);
 
+				// Name der datei
 		        String fileName = (attachment.getFileName() != null) ? attachment.getFileName() : attachment.getDisplayName();
+
+				// fileName wird bereinigt
 				fileName = clean_name(fileName);
+
+				// id wird in den fileName intigriert
 				String filePath = dir + id + "-" + fileName;
 
-				tmp[r] = new JSONObject();
+				// anlegung des neuen eintrags
+				attachment_obj[r] = new JSONObject();
 
+				/*
+					checkt ob die datei schon exsistiert
+
+					fuegt eine weitere zalh(tmp_counter) an den name,
+					wenn sie schon exsistiert.
+				*/
 				File test_file = new File(filePath);
-
 				if (test_file.exists())
 				{
 					int tmp_counter = 1;
 					String tmp_filePath = dir + id + "-" + tmp_counter + "-" + fileName;
 
+					// zaehlt hoch bis ein passender name gefunden wurde.
 					while (test_file.exists())
 					{
 						tmp_filePath = dir + id + "-" + tmp_counter + "-" + fileName;
@@ -186,37 +268,43 @@ public class PstMailJSON
 						tmp_counter = tmp_counter + 1;
 					}
 
+					// update filePath
 					filePath = tmp_filePath;
 				}
 
 				try
 				{
+					// speichern vom Attachment
 					attachment.save(filePath, true);
 				}
 				catch (IOException e)
 				{
 					System.err.println("IOException: get_attachments(Item inItem, long id)");
-					// System.err.println(fileName);
 					System.err.println("id: " + id + " ---> " + filePath);
 
-					// e.printStackTrace();
+					e.printStackTrace();
 				}
 
+				// filterung des daten typs
 				if (filePath.contains("."))
 				{
 					String type[] = filePath.split("\\.");
-					tmp[r].put("asset_type", type[type.length - 1]);
+					// letzer eintrag wird genommem (z.B: png)
+					attachment_obj[r].put("asset_type", type[type.length - 1]);
 				}
 				else
 				{
-					tmp[r].put("asset_type", "mail_attachment");
-					// tmp[r].put("type", "unknown");
-					// tmp[r].put("type", null);
+					/*
+						Setz asset_type zu mail_attachment
+						falls kein type festgestellt wurde.
+						Oft der fall bei E-Tickes
+					*/
+					attachment_obj[r].put("asset_type", "mail_attachment");
 				}
 
-				// tmp[r].put("asset_type", "mail_attachment");
-				tmp[r].put("path", Paths.get(filePath).toAbsolutePath().toString());
-				return_attachments.add(tmp[r]);
+				// follen System-path
+				attachment_obj[r].put("url", Paths.get(filePath).toAbsolutePath().toString());
+				return_attachments.add(attachment_obj[r]);
 		    }
 			return return_attachments;
 		}
@@ -226,6 +314,7 @@ public class PstMailJSON
 		}
 	}
 
+	// windows rtf body (UTF-8)
 	public static String get_rtf_body(Item inItem)
 	{
 		if (inItem.getBodyRtf() != null)
@@ -240,101 +329,142 @@ public class PstMailJSON
 		}
 	}
 
-	public static Map<String, String> get_mail(Item inItem)
-    {
-		Map<String, String> return_mail = new HashMap<String, String>();
-
+	// Mail Date
+	public static String get_mail_date(Item inItem)
+	{
 		if (inItem instanceof Message)
 		{
 			Message message = (Message) inItem;
 
-			return_mail.put("SenderName", message.getSenderName());
-			return_mail.put("SenderEmailAddress", message.getSenderEmailAddress());
-			return_mail.put("Date", convertDateToString(message.getClientSubmitTime()));
+			// formated return for kibana
+			return kibana_string_format(message.getClientSubmitTime());
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	// Mail Sender info + Date
+	public static JSONObject get_mail_sender(Item inItem)
+    {
+		// Rueckgabe JSONObject
+		JSONObject return_mail_header = new JSONObject();
+
+		// Checkt ob Komatibel
+		if (inItem instanceof Message)
+		{
+			Message message = (Message) inItem;
+
+			return_mail_header.put("name", message.getSenderName());
+			return_mail_header.put("email", message.getSenderEmailAddress());
         }
 		else
 		{
-			return_mail.put("SenderName", null);
-			return_mail.put("SenderEmailAddress", null);
-			return_mail.put("Date", null);
+			return_mail_header.put("name", null);
+			return_mail_header.put("email", null);
 		}
-		return return_mail;
+		return return_mail_header;
     }
 
-	public static Map<String, JSONArray> get_display(Item inItem)
+	// Cc Entfaenger
+	public static JSONArray get_displayCc(Item inItem)
 	{
-		Map<String, JSONArray> return_display = new HashMap<String, JSONArray>();
-
+		// Checkt ob Komatibel
 		if (inItem instanceof Message)
 		{
 			Message message = (Message) inItem;
 
-			String To = (String) message.getDisplayTo();
+			// Alle Entfaenger als ein String
 			String Cc = (String) message.getDisplayCc();
 
-			if (To != null)
-			{
-				JSONArray tmp_json_array_To = new JSONArray();
-
-				if (To.contains(";"))
-				{
-					String[] tmp_array = To.split("; ");
-
-					for(int x = 0; x < tmp_array.length; x++)
-					{
-						tmp_json_array_To.add(tmp_array[x]);
-					}
-
-				}
-				else
-				{
-					tmp_json_array_To.add(To);
-				}
-				return_display.put("DisplayTo", tmp_json_array_To);
-			}
-			else
-			{
-				return_display.put("DisplayTo", null);
-			}
-
+			// Nicht immer Cc Entfaenger
 			if (Cc != null)
 			{
-				JSONArray tmp_json_array_Cc = new JSONArray();
+				// Rueckgabe JsonArray
+				JSONArray return_displayCc = new JSONArray();
 
-				if (Cc.contains(";"))
+				// Spliten der Cc Entfaenger
+				if (Cc.contains("; "))
 				{
-					String[] tmp_array = Cc.split("; ");
-
-					for(int x = 0; x < tmp_array.length; x++)
+					// adden der Entfaenger an das Rueckgabe JsonArray
+					for(String entfaenger: Cc.split("; "))
 					{
-						tmp_json_array_Cc.add(tmp_array[x]);
+						return_displayCc.add(entfaenger);
 					}
-
 				}
 				else
 				{
-					tmp_json_array_Cc.add(Cc);
+					// Nur ein Cc Entfaenger
+					return_displayCc.add(Cc);
 				}
-
-				return_display.put("DisplayCc", tmp_json_array_Cc);
+				return return_displayCc;
 			}
 			else
 			{
-				return_display.put("DisplayCc", null);
+				// Keine Cc Entfaenger
+				return null;
 			}
-
 		}
 		else
 		{
-			return_display.put("DisplayTo", null);
-			return_display.put("DisplayCc", null);
+			// Keine Cc Entfaenger
+			return null;
 		}
-		return return_display;
+	}
+
+	// Cc Entfaenger
+	public static JSONArray get_displayTo(Item inItem)
+	{
+		// Checkt ob Komatibel
+		if (inItem instanceof Message)
+		{
+			Message message = (Message) inItem;
+
+			// Alle Entfaenger als ein String
+			String To = (String) message.getDisplayTo();
+
+			// Nicht immer To Entfaenger
+			if (To != null)
+			{
+				// Rueckgabe JsonArray
+				JSONArray return_displayTo = new JSONArray();
+
+				// Spliten der To Entfaenger
+				if (To.contains("; "))
+				{
+					// adden der Entfaenger an das Rueckgabe JsonArray
+					for(String entfaenger: To.split("; "))
+					{
+						return_displayTo.add(entfaenger);
+					}
+				}
+				else
+				{
+					// Nur ein To Entfaenger
+					return_displayTo.add(To);
+				}
+				return return_displayTo;
+			}
+			else
+			{
+				// Keine To Entfaenger
+				return null;
+			}
+		}
+		else
+		{
+			// Keine To Entfaenger
+			return null;
+		}
 	}
 
 	public static void main(String[] args)
 	{
+		// pst soucre Datei
 		String file_name = args[0];
+
+		// json ablage Ordner
 		String file_folder = "./data/";
 
         try
@@ -343,90 +473,118 @@ public class PstMailJSON
 
             try
             {
+				// Alle Ordner in der pst Datei --> anzahl
                 List<Folder> folders = file.getMailboxRoot().getFolders(true);
+
+				// tmp_id zur performant Verbesserung bei der neuen id Vergabe
 				long tmp_id = 1;
 
+				// Alle Ordner durchsuchen
 				for (int x = 0; x < folders.size(); x++)
 				{
 					try
 					{
+						// Ordner mit Mail(Items)
 						Folder inbox = folders.get(x);
 
+						// Ausgabe des Ordners und siener Groesse
 						System.out.format("%5d %-30s\n", inbox.getItemCount(), inbox.getDisplayName());
 
+						// Checkt ob Ordner Daten inthaelt
 			        	if (inbox != null)
 			            {
+							// Liste mit den Mails (Items) im Ordner inbox
 			            	List<Item> items = inbox.getItems();
 
+							// durchgehen aller Items
 			                for (int i = 0; i < items.size(); i++)
 			                {
+								// id der Mail (Item)
 								long id = items.get(i).getId();
 
+								/*
+									Checkt ob die json datei schon im
+									file_folder exsistiert und generiet
+									ggf. eine neue.
+								*/
 								File test_file = new File(file_folder + id + ".json");
-
 								if (test_file.exists())
 								{
+									/*
+										Checkt ob die tmp_id im
+										file_folder exsistiert.
+									*/
 									test_file = new File(file_folder + tmp_id + ".json");
 
+									/*
+										Zaehlt solange hoch bis
+										eine passende id gefunden wurde.
+									*/
 									while (test_file.exists())
 									{
 										tmp_id = tmp_id + 1;
 										test_file = new File(file_folder + tmp_id + ".json");
 									}
+
+									// update tmp_id mit id
 									id = tmp_id;
 									// System.out.print("New id: --> " + id + "\r");
 								}
 
+								// output file mit id als namen im file_folder
 								PrintWriter output_file = new PrintWriter(file_folder + id + ".json", "UTF-8");
 
-								JSONObject jsonOBJ = new JSONObject();
+								// JsonObject was in output_file geschrieben wird
+								JSONObject root_json = new JSONObject();
 
-								jsonOBJ.put("id", id);
-								jsonOBJ.put("Subject", items.get(i).getSubject());
-								jsonOBJ.put("Plain_body", items.get(i).getBody());
-								jsonOBJ.put("Html_body", items.get(i).getBodyHtmlText());
+								// daten die ohne initialisation entnommen werden.
+								root_json.put("id", id);
+								root_json.put("Subject", items.get(i).getSubject());
+								root_json.put("Plain_body", items.get(i).getBody());
+								root_json.put("Html_body", items.get(i).getBodyHtmlText());
 
-								Map<String, Date> task = get_task(items.get(i));
+								/*
+									Task get_task
+									Datum wird als String uebergeben
+								*/
+								Map<String, String> task = get_task(items.get(i));
 
-								jsonOBJ.put("Task_StartTime", convertDateToString(task.get("Task StartTime")));
-								jsonOBJ.put("Task_EndTime", convertDateToString(task.get("Task EndTime")));
+								root_json.put("Task_StartTime", task.get("Task_StartTime"));
+								root_json.put("Task_EndTime", task.get("Task_EndTime"));
 
-								Map<String, Date> appointment = get_appointment(items.get(i));
+								/*
+								 	Appointment get_appointment
+									Datum wird als String uebergeben
+								*/
+								Map<String, String> appointment = get_appointment(items.get(i));
 
-								jsonOBJ.put("Appointment_StartTime", convertDateToString(appointment.get("Appointment StartTime")));
-								jsonOBJ.put("Appointment_EndTime", convertDateToString(appointment.get("Appointment EndTime")));
+								root_json.put("Appointment_StartTime", appointment.get("Appointment_StartTime"));
+								root_json.put("Appointment_EndTime", appointment.get("Appointment_EndTime"));
 
-								Map<String, String> contact = get_contact(items.get(i));
-								JSONObject contact_JSON = new JSONObject();
+								// Kontaktdaten
+								root_json.put("Contact", get_contact(items.get(i)));
 
-								contact_JSON.put("name", contact.get("GivenName"));
-								contact_JSON.put("email", contact.get("Email1Address"));
-								contact_JSON.put("email_name", contact.get("Email1DisplayName"));
-								contact_JSON.put("BusinessPhone", contact.get("BusinessPhone"));
-								contact_JSON.put("BusinessAddress", contact.get("BusinessAddress"));
+								// Attachments
+								root_json.put("Attachments", get_attachments(items.get(i), id));
 
-								jsonOBJ.put("Contact", contact.get("BusinessAddress"));
+								// rtf body
+								root_json.put("Rtf_body", get_rtf_body(items.get(i)));
 
-								jsonOBJ.put("Attachments", get_attachments(items.get(i), id));
-								jsonOBJ.put("Rtf_body", get_rtf_body(items.get(i)));
+								// Sender info
+								root_json.put("Sender", get_mail_sender(items.get(i)));
 
-								Map<String, String> mail = get_mail(items.get(i));
-								JSONObject mail_JSON = new JSONObject();
+								// Mail date
+								root_json.put("Date", get_mail_date(items.get(i)));
 
-								mail_JSON.put("name", mail.get("SenderName"));
-								mail_JSON.put("email", mail.get("SenderEmailAddress"));
+								// Entfaenger
+								root_json.put("DisplayTo", get_displayTo(items.get(i)));
+								root_json.put("DisplayCc", get_displayCc(items.get(i)));
 
-								jsonOBJ.put("Sender", mail_JSON);
-								jsonOBJ.put("Date", mail.get("Date"));
+								// Entfaenger detail
+								root_json.put("Recipier", get_recipier(items.get(i)));
 
-								Map<String, JSONArray> display = get_display(items.get(i));
-
-								jsonOBJ.put("DisplayTo", display.get("DisplayTo"));
-								jsonOBJ.put("DisplayCc", display.get("DisplayCc"));
-
-								jsonOBJ.put("Recipier", get_recipier(items.get(i)));
-
-								output_file.println(jsonOBJ);
+								// schriebt alle Mail infos in output_file
+								output_file.println(root_json);
 								output_file.close();
 							}
 			        	}
--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -Xmx12g -cp .:jpst-1.0.jar:json-simple-1.1.1.jar PstMailJSON $1
-# find /Users/hacker/Desktop/Hacked\ Team/ -name "*.pst" -exec echo {} \; -exec java -Xmx12g -cp .:jpst-1.0.jar:json-simple-1.1.1.jar PstMailJSON {} \;
+# java -Xmx12g -cp .:jpst-1.0.jar:json-simple-1.1.1.jar PstMailJSON $1
+find /Users/hacker/Desktop/Hacked-Team/ -name "*.pst" -exec echo {} \; -exec java -Xmx12g -cp .:jpst-1.0.jar:json-simple-1.1.1.jar PstMailJSON {} \;
